b824b1567bdf5f7c3dcecf86a39e44ad1c664bc4
Rafael Seemann <rseemann@gmail.com>
2013-12-23 22:54:21 -0200

    Eventos de teclado feitos

--- src/camera_controller.js
+++ src/camera_controller.js
@@ -2,7 +2,8 @@ define([
   'jquery',
   'underscore'
 ], function ($, _) {
-  var STEP = 0.1;
+  var POS_STEP = 0.1;
+  var ROT_STEP = 0.1;
 
   var CameraController = function (options) {
     this.camera = options.camera;
@@ -22,26 +23,41 @@ define([
   }
 
   function onKeydown (event) {
-    console.log(event.keyCode);
-
     switch(event.keyCode){
       case 65://a
-        moveCamera.call(this, 'x', -STEP);
+        moveCamera.call(this, 'x', -POS_STEP);
         break;
       case 68://d
-        moveCamera.call(this, 'x', +STEP);
+        moveCamera.call(this, 'x', +POS_STEP);
         break;
       case 83://s
-        moveCamera.call(this, 'z', +STEP);
+        moveCamera.call(this, 'z', +POS_STEP);
         break;
       case 87://w
-        moveCamera.call(this, 'z', -STEP);
+        moveCamera.call(this, 'z', -POS_STEP);
         break;
+      case 37://left
+        rotateCamera.call(this, 'y', +ROT_STEP);
+        break;
+      case 39://right
+        rotateCamera.call(this, 'y', -ROT_STEP);
+        break;
+      case 38://up
+        rotateCamera.call(this, 'x', +ROT_STEP);
+        break;
+      case 40://down
+        rotateCamera.call(this, 'x', -ROT_STEP);
+        break;
+
     }
   }
 
   function moveCamera (axis, value) {
     this.camera.position[axis] += value;
+  }
+
+  function rotateCamera (axis, value) {
+    this.camera.rotation[axis] += value;
   }
 
   return CameraController;
